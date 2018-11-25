53c5cc77fbf3ab1aba565f43e3d28a4ff4ea0614
Loek van Gool <loekvangool@gmail.com>
2018-11-25 16:43:24 +0100

    Check whether the cache needs a rebuild before rebuilding it

--- shell/rebuild_cache.php
+++ shell/rebuild_cache.php
@@ -8,15 +8,23 @@ Mage::app('default');
 Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
 
 try {
+  $invalidated_types = Mage::app()->getCacheInstance()->getInvalidatedTypes();
+  $invalidated_ids = [];
+  foreach($invalidated_types as $invalidated_type) {
+    $invalidated_ids[] = $invalidated_type->getData('id');
+  }
+
   $allTypes = Mage::app()->useCache();
   foreach($allTypes as $type => $value) {
-    if(in_array($type, $types)) {
+    if(in_array($type, $types) && in_array($type, $invalidated_ids)) {
       Mage::app()->getCacheInstance()->cleanType($type);
       Mage::dispatchEvent('', array('type' => $type));
-      echo PHP_EOL."Rebuilt {$type}".PHP_EOL;
+      echo "Rebuilt {$type}".PHP_EOL;
+    } else {
+      echo "Skipped {$type}".PHP_EOL;
     }
   }
-  echo 'done';
+  echo 'Done'.PHP_EOL;
 } catch (Exception $e) {
   echo $e->getMessage();
 }
