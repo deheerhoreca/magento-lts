5f6edf5e7c29e81a6d1c708804ae378647b7f956
Loek van Gool <loekvangool@gmail.com>
2018-11-30 20:22:39 +0100

    Marking 2 core hacks, restoring one file to original

--- app/Mage.php
+++ app/Mage.php
@@ -53,6 +53,7 @@ if (defined('COMPILER_INCLUDE_PATH')) {
 
 Varien_Autoload::register();
 
+/* DHH CORE HACK */
 /** AUTOLOADER PATCH **/
 if (file_exists($autoloaderPath = BP . DS . 'vendor/autoload.php') ||
     file_exists($autoloaderPath = BP . DS . '../vendor/autoload.php')
@@ -60,6 +61,7 @@ if (file_exists($autoloaderPath = BP . DS . 'vendor/autoload.php') ||
     require $autoloaderPath;
 }
 /** AUTOLOADER PATCH **/
+/* DHH CORE HACK */
 
 /**
  * Main Mage hub class
