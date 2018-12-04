7999a14e59095995530202fa0703ee1e24dda6e9
Loek van Gool <loekvangool@gmail.com>
2018-12-04 11:59:06 +0100

    Adding external libs

--- shell/download_external_libs.sh
+++ shell/download_external_libs.sh
@@ -5,3 +5,15 @@
 
 mkdir -p ../skin/frontend/rwd/external/opensans
 wget https://gist.github.com/stefanmaric/a5043c0998d9fc35483d/raw/55afaea418aaee1d074b4a427496573c5e8e5200/open-sans.css -O ../skin/frontend/rwd/external/opensans/open-sans.css
+
+mkdir -p ../skin/frontend/rwd/external/clickcease/monitor
+wget https://www.clickcease.com/monitor/stat.js -O ../skin/frontend/rwd/external/clickcease/monitor/stat.js
+
+mkdir -p ../skin/frontend/rwd/external/hotjar
+wget https://static.hotjar.com/c/hotjar-943471.js?sv=6 -O ../skin/frontend/rwd/external/hotjar/hotjar-943471.js
+
+mkdir -p ../skin/frontend/rwd/external/ga
+wget https://www.google-analytics.com/analytics.js -O ../skin/frontend/rwd/external/ga/analytics.js
+
+mkdir -p ../skin/frontend/rwd/external/zopim
+wget https://v2.zopim.com/?5GFYzGgeADrvMUAoHWiZPPglkc93U381  -O ../skin/frontend/rwd/external/zopim/widget.js
