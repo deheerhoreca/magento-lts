30f37b010dcb8587f2c338c66a2c770cf0cc7262
Loek van Gool <loekvangool@gmail.com>
2019-06-02 20:32:09 +0200

    Improvements in the QND FPC logic

--- app/design/frontend/rwd/dhh/template/page/dhhfpc_conf.php
+++ app/design/frontend/rwd/dhh/template/page/dhhfpc_conf.php
@@ -1,3 +1,10 @@
 <?php
 
-define("DHH_FPC_ENABLED", FALSE);
+define("DHH_FPC_ENABLED", true);
+
+/*
+- 1column.phtml:
+  - https://www.prokoeling.nl/koelkasten/alle-koelkasten/display-koelkasten/display-koelkast-zwart-wit-met-slot-klapdeuren-800ltr-exquisit.html
+- 2columns-left.phtml:
+  - https://www.prokoeling.nl/koelkasten/alle-koelkasten.html
+*/
