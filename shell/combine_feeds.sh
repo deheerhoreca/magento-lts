835049d1b1b9a5de5518deb9ec44ba76ae3cfa96
Loek van Gool <loekvangool@gmail.com>
2019-11-01 18:28:49 +0100

    Script to combine XML feeds into a single one

--- shell/combine_feeds.sh
+++ shell/combine_feeds.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
-cd ../media/amfeed/feeds
-rm -f combined.xml
-touch combined.xml
+# Marktplaats requires all feeds in a single file
 
-wget https://www.prokoeling.nl/amfeed/main/get/file/prokoeling_034FA_default_100less/?___store=default -O ->> combined.xml
-wget https://www.prokoeling.nl/amfeed/main/get/file/prokoeling_034FA_default_100/?___store=default -O ->> combined.xml
-wget https://www.prokoeling.nl/amfeed/main/get/file/prokoeling_034FA_other/?___store=default -O ->> combined.xml
-wget https://www.prokoeling.nl/amfeed/main/get/file/prokoeling_034FA_koelingen/?___store=default -O ->> combined.xml
+cd ../media/amfeed/feeds
+rm -f combined_fixed.xml
+touch combined_fixed.xml
+
+cp export_5 _combine_5.tmp.xml
+cp export_6 _combine_6.tmp.xml
+cp export_7 _combine_7.tmp.xml
+cp export_8 _combine_8.tmp.xml
+
+# Remove first line
+sed -i -e "1d" _combine_6.tmp.xml
+sed -i -e "1d" _combine_7.tmp.xml
+sed -i -e "1d" _combine_8.tmp.xml
+
+# Remove last 2 closing tags
+sed -i 's/<\/channel><\/rss>//g' _combine_5.tmp.xml
+sed -i 's/<\/channel><\/rss>//g' _combine_6.tmp.xml
+sed -i 's/<\/channel><\/rss>//g' _combine_7.tmp.xml
+
+cat _combine_5.tmp.xml _combine_6.tmp.xml _combine_7.tmp.xml _combine_8.tmp.xml > combined_fixed.xml
+
+rm _combine_*.tmp.xml
