ff4807a589467a65351950fd2bdb035ec502b5ab
Loek van Gool <loekvangool@gmail.com>
2020-10-28 13:13:27 +0100

    Updating to new PHP version

--- shell/clear_cache.sh
+++ shell/clear_cache.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/opt/plesk/php/7.2/bin/php -r 'require "app/Mage.php"; Mage::app()->getCacheInstance()->flush();'
+/opt/plesk/php/7.3/bin/php -r 'require "app/Mage.php"; Mage::app()->getCacheInstance()->flush();'
