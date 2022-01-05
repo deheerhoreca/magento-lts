46549fc72a9ca67833f9636728c1ad067346edc7
Loek <loek@deheerhoreca.nl>
2022-01-05 17:04:34 +0100

    New tool to convert CMYK to RGP due to Bol.com issue

---
+++ shell/cmyk_to_rgb.sh
@@ -0,0 +1,17 @@
+
+pattern=~/httpdocs/deheerhoreca-magento/media/catalog/product/{0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f,g,h,i,j,k,l,m,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,O,P,Q,R,S,T,U,V,W,X,Y,Z,haha}/*/*.jpg
+
+pattern=~/httpdocs/deheerhoreca-magento/media/catalog/product/haha/*.jpg
+
+for f in ${pattern}
+  do
+    type=$(identify -format '%[colorspace]' $f);
+    if [ "$type" == "CMYK" ]
+    then
+      echo Convert ${type} ${f}
+      # convert $f -colorspace RGB ./$
+      convert  $f -colorspace CMYK -colorspace RGB $f # uncomment this line and comment previous line if previous line is not working.
+    else
+      echo ${type} ${f}
+    fi
+  done
