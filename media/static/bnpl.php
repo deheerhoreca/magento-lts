089c874a88f795fb0bdb47ba84b563f39e4c8dee
Loek <loek@deheerhoreca.nl>
2022-11-10 15:15:22 +0100

    Removing Billink references

--- media/static/bnpl.php
+++ media/static/bnpl.php
@@ -7,7 +7,7 @@
   </head>
   <body>
     <div style="padding: 3em;">
-      <div id="info-billink" class="std">
+      <div class="std">
         
         <div id="info-biller" class="std">
           <img src="/skin/frontend/rwd/dhh/images/biller-logo.png" alt="Biller logo" title="Biller" style="width:140px; height:50px; float:right; margin-top: .5em;" />
@@ -20,19 +20,6 @@
           <div style="max-width: 600px;">
             <p>Wanneer u hiervoor kiest, zorgt Biller voor het gehele achteraf betalen proces. Het voordeel van Biller is dat u tot 30 dagen na levering kan betalen tot een bedrag van €5.000 inclusief BTW. Biller is speciaal gemaakt voor de zakelijke markt.</p>
             <p><strong><u>Let op: Als u gebruik wil maken van Biller, maak dan een Offerte aan</u></strong>.</p>
-          </div>
-        </div>
-        
-        <div id="info-billink" class="std">
-          <img src="/skin/frontend/rwd/dhh/images/billink_logo.png" alt="Billink logo" title="Billink" style="width:140px; height:39px; float:right; margin-top: .5em;" />
-          <h2>🤩Achteraf betalen met Billink</h2>
-          <h5><i>In Nederland voor zakelijke en particuliere klanten</i></h5>
-          <h4>Maximaal €3500 (zakelijk), €750 (particulier), incl. BTW</h4>
-          <div style="max-width: 550px;">
-            <strong>Met Billink betaal je het volledige bedrag zonder rente binnen 14 dagen.</strong>
-          </div>
-          <div style="max-width: 550px;">
-            <p>Bestel nu online, betaal achteraf. Betalen met Billink is heel gemakkelijk. Je bestelt wat je nodig hebt en selecteert tijdens het betaalproces dat je wilt betalen met Billink. Wil je graag een zakelijke bestelling achteraf betalen? Van Billink ontvang je een handige factuur, die je vervolgens binnen 14 dagen betaalt.</p>
           </div>
         </div>
         
