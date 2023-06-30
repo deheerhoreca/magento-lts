6f47daa0fc40c636f6ec305ac5b8d2a2d814f0ef
Loek van Gool <loekvangool@gmail.com>
2023-06-30 10:41:42 +0200

    Removing leftovers, and some fixes, one core hack

--- lib/Varien/Crypt/Mcrypt.php
+++ lib/Varien/Crypt/Mcrypt.php
@@ -126,7 +126,9 @@ class Varien_Crypt_Mcrypt extends Varien_Crypt_Abstract
 
     protected function _reset()
     {
-        mcrypt_generic_deinit($this->getHandler());
-        mcrypt_module_close($this->getHandler());
+      // DHH CORE HACK -- Patched https://github.com/OpenMage/magento-lts/pull/2968
+        $handler = $this->getHandler();
+        mcrypt_generic_deinit($handler);
+        mcrypt_module_close($handler);
     }
 }
