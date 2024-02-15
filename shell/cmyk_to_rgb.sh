67252f53fe82f0603fb14512dceaad5068e0e582
loek <loek@deheerhoreca.nl>
2024-02-15 12:14:48 +0100

    fixes from voyager

--- shell/cmyk_to_rgb.sh
+++ shell/cmyk_to_rgb.sh
@@ -1,25 +1,40 @@
+#!/bin/bash
 
-pattern=~/httpdocs/deheerhoreca-magento/media/catalog/product/{0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f,g,h,i,j,k,l,m,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,O,P,Q,R,S,T,U,V,W,X,Y,Z,haha}/*/*.jpg
+# ~/httpdocs/deheerhoreca-magento/shell/cmyk_to_rgb.sh
 
-pattern=~/httpdocs/deheerhoreca-magento/media/catalog/product/haha/*.jpg
+shopt -s nocaseglob
+
+cd ~/httpdocs/deheerhoreca-magento
+
+# PATTERN=~/httpdocs/deheerhoreca-magento/media/catalog/product/{0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f,g,h,i,j,k,l,m,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,O,P,Q,R,S,T,U,V,W,X,Y,Z,haha}/*/*.jpg
+PATTERN=~/httpdocs/deheerhoreca-magento/media/catalog/product/haha/*.jpg
 
 DRYRUN=true
 
-for f in ${pattern}
-  do
-    type=$(identify -format '%[colorspace]' $f);
-    if [ "$type" == "CMYK" ]
-    then
-      echo "${type} ${f} NOK"
-      if [ "${DRYRUN}" == false ]
+if [ "${DRYRUN}" == true ]; then
+  echo "Dryrun: Printing commands only."
+fi
+
+echo "Current pattern: ${PATTERN}"
+sleep 1
+
+# Subshell, in order to set case insensitive but no risk that the setting will be kept in the interactive session
+(shopt -s nocaseglob; 
+  for f in ${PATTERN}
+    do
+      TYPE=$(identify -format '%[colorspace]' $f);
+      if [ "$TYPE" == "CMYK" ]
       then
-        convert $f -colorspace RGB ./$
-        # convert $f -colorspace CMYK -colorspace RGB $f # uncomment this line and comment previous line if previous line is not working.
-        echo "$f -colorspace RGB";
+        echo "${TYPE} ${f} NOK"
+        if [ "${DRYRUN}" == false ]; then
+          convert $f -colorspace RGB ./$
+          # convert $f -colorspace CMYK -colorspace RGB $f # uncomment this line and comment previous line if previous line is not working.
+          echo "$f -colorspace RGB";
+        else
+          echo "convert $f -colorspace RGB";
+        fi
       else
-        echo "Dryrun: $f -colorspace RGB";
+        echo "${TYPE} ${f} OK"
       fi
-    else
-      echo "${type} ${f} OK"
-    fi
-  done
+    done
+)
