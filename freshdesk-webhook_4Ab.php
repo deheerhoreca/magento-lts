c871fe977f236c664941f80b7af1c9fefe6a6108
Loek <loek@deheerhoreca.nl>
2024-02-22 01:06:04 +0100

    Updates from production

---
+++ freshdesk-webhook_4Ab.php
@@ -0,0 +1,36 @@
+<?php
+
+$output = __DIR__."/freshdesk-webhook.ndjson";
+
+$json = file_get_contents("php://input");
+$data = json_decode($json, true);
+
+$data = [
+  "@timestamp"    => date("c"),
+  "remote_addr"   => $_SERVER['REMOTE_ADDR'],
+  "forwarded_for" => ($_SERVER['HTTP_X_FORWARDED_FOR'] ?? null),
+  "payload"       => (array) $data,
+  "process"       => [],
+];
+
+// Store event in log
+// print_r($json);
+$json = json_encode($data).PHP_EOL;
+
+try {
+  touch($output);
+  if(file_put_contents($output, $json, FILE_APPEND)) {
+    http_response_code(202);
+    echo "OK";
+    exit;
+  } else {
+    http_response_code(500);
+  }
+} catch(Exception $e) {
+  $json = json_encode($e->getMessage());
+  if(file_put_contents($output_err, $json, FILE_APPEND)) {
+    http_response_code(500);
+  } else {
+    http_response_code(500);
+  }
+}
--- shell/cmyk_to_rgb.sh
+++ shell/cmyk_to_rgb.sh
@@ -7,7 +7,7 @@ shopt -s nocaseglob
 cd ~/httpdocs/deheerhoreca-magento
 
 # PATTERN=~/httpdocs/deheerhoreca-magento/media/catalog/product/{0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f,g,h,i,j,k,l,m,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,O,P,Q,R,S,T,U,V,W,X,Y,Z,haha}/*/*.jpg
-PATTERN=~/httpdocs/deheerhoreca-magento/media/catalog/product/haha/*.jpg
+PATTERN=~/httpdocs/deheerhoreca-magento/media/catalog/product/*/*.jpg
 
 DRYRUN=true
 
@@ -34,7 +34,8 @@ sleep 1
           echo "convert $f -colorspace RGB";
         fi
       else
-        echo "${TYPE} ${f} OK"
+        # echo "${TYPE} ${f} OK"
+        printf "."
       fi
     done
 )
