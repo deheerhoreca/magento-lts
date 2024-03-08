1502452a4ee634bce08654c478ccf4fed43d3352
Loek van Gool <loekvangool@gmail.com>
2024-03-08 12:31:46 +0100

    Improvements to rector config

--- dev/rector-3rdparty.php
+++ dev/rector-3rdparty.php
@@ -23,15 +23,29 @@ return static function (Rector\Config\RectorConfig $rectorConfig): void {
   // $rectorConfig->disableParallel();
   
   $rectorConfig->paths([
-    __DIR__ . "/../*.php",
-    __DIR__ . "/../app",
-    __DIR__ . "/../lib",
+    __DIR__ . "/../app/code/community",
+    __DIR__ . "/../app/code/local",
+    __DIR__ . "/../lib/Afterpay",
+    __DIR__ . "/../lib/Amasty",
+    __DIR__ . "/../lib/Ebizmarts",
+    __DIR__ . "/../lib/FireGento",
+    __DIR__ . "/../lib/Google",
+    __DIR__ . "/../lib/Mandrill",
+    __DIR__ . "/../lib/MaxMind",
+    __DIR__ . "/../lib/Mpdf",
+    __DIR__ . "/../lib/n98-magerun",
+    __DIR__ . "/../lib/Net",
+    __DIR__ . "/../lib/SimpleHtmlDoom",
+    __DIR__ . "/../lib/TM",
+    __DIR__ . "/../tool_apc.php",
+    __DIR__ . "/../tool_opc.php",
+    __DIR__ . "/../tool_redis.php",
   ]);
   
   $rectorConfig->skip([
-    __DIR__ . "/../vendor",
-    __DIR__ . "/../media",
-    __DIR__ . "/../tool_*.php",
+    // __DIR__ . "/../vendor",
+    // __DIR__ . "/../media",
+    // __DIR__ . "/../tool_*.php",
     __DIR__ . "/../lib/TM/Geoip/vendor",
     __DIR__ . "/../lib/Afterpay/vendor",
     
@@ -44,19 +58,26 @@ return static function (Rector\Config\RectorConfig $rectorConfig): void {
     Rector\DeadCode\Rector\If_\RemoveAlwaysTrueIfConditionRector::class,
     Rector\DeadCode\Rector\Switch_\RemoveDuplicatedCaseInSwitchRector::class,
     Rector\CodeQuality\Rector\FunctionLike\SimplifyUselessVariableRector::class,
-    Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector::class,              // Harder to read code
+    Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector::class,                    // Harder to read code
     Rector\TypeDeclaration\Rector\ClassMethod\ReturnNeverTypeRector::class,
-    Rector\Php54\Rector\Array_\LongArrayToShortArrayRector::class,                // Harder to read code
+    Rector\Php54\Rector\Array_\LongArrayToShortArrayRector::class,                      // Harder to read code
     
-    // Sometimes not desirable:
-    Rector\Php80\Rector\Switch_\ChangeSwitchToMatchRector::class,
-    // Rector\Php71\Rector\FuncCall\RemoveExtraParametersRector::class,
+    // Enable these to do only minimally invasive changes (3rd party code): 
+    Rector\Php80\Rector\Switch_\ChangeSwitchToMatchRector::class,                       // Harder to read code
+    Rector\Php71\Rector\ClassConst\PublicConstantVisibilityRector::class,               // Not necessary for 3rd party code
+    Rector\Php53\Rector\Ternary\TernaryToElvisRector::class,                            // Not necessary for 3rd party code
+    Rector\Php70\Rector\StmtsAwareInterface\IfIssetToCoalescingRector::class,           // Not necessary for 3rd party code
+    Rector\Php80\Rector\FunctionLike\MixedTypeRector::class,                            // Non-sensical for 3rd party code
+    Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector::class,  // Too invasive
+    Rector\Php71\Rector\FuncCall\RemoveExtraParametersRector::class,                    // Too invasive
+    Rector\Php80\Rector\Catch_\RemoveUnusedVariableInCatchRector::class,                // Too invasive
   ]);
   
   // Register a single rule:
   // $rectorConfig->rule(Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector::class);
   // $rectorConfig->rule(Rector\Php54\Rector\Array_\LongArrayToShortArrayRector::class);
   // $rectorConfig->rule(Rector\Php73\Rector\FuncCall\StringifyStrNeedlesRector::class);
+  // $rectorConfig->rule(Rector\Php80\Rector\NotIdentical\StrContainsRector::class);
   
   // Define sets of rules
   $rectorConfig->sets([
