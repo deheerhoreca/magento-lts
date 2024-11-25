095ea3b7874529d97e381902db9d3966263e98f2
Loek <loek@deheerhoreca.nl>
2024-11-25 23:26:16 +0100

    update from production

--- shell/fix-permissions.sh
+++ shell/fix-permissions.sh
@@ -5,11 +5,13 @@
 # Set User Environment
 . ${HOME}/.bash_profile
 
-cm
-
 set -e      # Exit immediately if a command exits with a non-zero status
 set -u      # Treat unset variables as an error when substituting
 
-cd ./media/catalog/product
+cm && cd media/catalog/product
+
+find . -type f ! -perm 0644 -print -exec chmod 0644 -- {} +
+
+cm && cd media/catalog/category
 
 find . -type f ! -perm 0644 -print -exec chmod 0644 -- {} +
