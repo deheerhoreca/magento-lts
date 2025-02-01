0413f9e167d8119f7726fa5378da429e16f55bd5
Loek van Gool <loekvangool@gmail.com>
2025-02-01 14:28:11 +0100

    update from dev

--- dev/rector.php
+++ dev/rector.php
@@ -2,22 +2,63 @@
 
 declare(strict_types=1);
 
+use Rector\CodeQuality\Rector as CodeQuality;
+use Rector\DeadCode\Rector as DeadCode;
+use Rector\TypeDeclaration\Rector as TypeDeclaration;
+use Rector\Config\RectorConfig;
+use Rector\ValueObject\PhpVersion;
+use Rector\Caching\ValueObject\Storage\FileCacheStorage;
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Set\ValueObject\SetList;
 use Rector\Set\ValueObject\LevelSetList;
+use Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector;
+use Rector\DeadCode\Rector\Assign\RemoveUnusedVariableAssignRector;
+use Rector\DeadCode\Rector\If_\RemoveAlwaysTrueIfConditionRector;
+use Rector\DeadCode\Rector\Switch_\RemoveDuplicatedCaseInSwitchRector;
+use Rector\DeadCode\Rector\Foreach_\RemoveUnusedForeachKeyRector;
+use Rector\Php82\Rector\Encapsed\VariableInStringInterpolationFixerRector;
+use Rector\Php80\Rector\Switch_\ChangeSwitchToMatchRector;
+use Rector\Strict\Rector\Empty_\DisallowedEmptyRuleFixerRector;
+use Rector\CodeQuality\Rector\Empty_\SimplifyEmptyCheckOnEmptyArrayRector;
+use Rector\CodeQuality\Rector\FunctionLike\SimplifyUselessVariableRector;
+use Rector\CodeQuality\Rector\FuncCall\SingleInArrayToCompareRector;
+use Rector\CodeQuality\Rector\If_\CombineIfRector;
+use Rector\CodeQuality\Rector\If_\CompleteMissingIfElseBracketRector;
+use Rector\CodeQuality\Rector\If_\ExplicitBoolCompareRector;
+use Rector\CodeQuality\Rector\Ternary\SwitchNegatedTernaryRector;
+use Rector\CodeQuality\Rector\Identical\SimplifyBoolIdenticalTrueRector;
+use Rector\CodeQuality\Rector\If_\SimplifyIfElseToTernaryRector;
+use Rector\CodeQuality\Rector\If_\ShortenElseIfRector;
+use Rector\Renaming\Rector\FuncCall\RenameFunctionRector;
+use Rector\DeadCode\Rector\Cast\RecastingRemovalRector;
+use Rector\Php71\Rector\FuncCall\RemoveExtraParametersRector;
+use Rector\Php54\Rector\Array_\LongArrayToShortArrayRector;
+use Rector\Transform\Rector\FuncCall\FuncCallToConstFetchRector;
+use Rector\CodingStyle\Rector\Stmt\NewlineAfterStatementRector;
+use Rector\CodingStyle\Rector\FuncCall\CountArrayToEmptyArrayComparisonRector;
+use Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector;
+use Rector\CodingStyle\Rector\Assign\SplitDoubleAssignRector;
+use Rector\CodingStyle\Rector\String_\SymplifyQuoteEscapeRector;
+use Rector\CodingStyle\Rector\Encapsed\WrapEncapsedVariableInCurlyBracesRector;
+use Rector\EarlyReturn\Rector\If_\ChangeOrIfContinueToMultiContinueRector;
+use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\ReturnNeverTypeRector;
 
-return static function (Rector\Config\RectorConfig $rectorConfig): void {
+return static function (RectorConfig $rectorConfig): void {
   
   // Specify a path that works locally as well as on CI job runners.
-  $uid = posix_getuid();
+  $uid        = posix_getuid();
   $shell_user = posix_getpwuid($uid);
-  $home_dir = $shell_user["dir"] ?? "~";
-  $cache_dir = "{$home_dir}/tmp/rector/".basename(realpath(__DIR__."/.."));
   
   $rectorConfig->indent(" ", 2);
-  // $rectorConfig->phpVersion(Rector\ValueObject\PhpVersion::PHP_81); // Use if different then composer.json
-  $rectorConfig->parallel($seconds = 600, $maxNumberOfProcess = 12, $jobSize = 30);
+  $rectorConfig->parallel($seconds = 600, $maxNumberOfProcess = 6, $jobSize = 30);
   $rectorConfig->fileExtensions(["phtml", "php"]);
-  $rectorConfig->cacheClass(Rector\Caching\ValueObject\Storage\FileCacheStorage::class);
+  $rectorConfig->cacheClass(FileCacheStorage::class);
+  $rectorConfig->cacheDirectory("/dev/shm/openmage/rector");
+  $rectorConfig->importNames();
+  
+  $rectorConfig->phpVersion(PhpVersion::PHP_83);          // Only when deviating from composer.json
+  $rectorConfig->removeUnusedImports(false);              // Maybe interesting sometimes
   
   // In case of errors:
   // $rectorConfig->disableParallel();
@@ -29,28 +70,44 @@ return static function (Rector\Config\RectorConfig $rectorConfig): void {
   ]);
   
   $rectorConfig->skip([
+    __DIR__ . "/../**/vendor",
+    // __DIR__ . "/../app/code/community/Pay/Payment/vendor",
+    // __DIR__ . "/../lib/Afterpay/vendor",
+    // __DIR__ . "/../lib/TM/Geoip/vendor",
+    __DIR__ . "/../protected",
     // __DIR__ . "/../vendor",
-    __DIR__ . "/../media",
-    __DIR__ . "/../tool_*.php",
-    __DIR__ . "/../lib/TM/Geoip/vendor",
-    __DIR__ . "/../lib/Afterpay/vendor",
-    
-    // These are bad:
-    Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector::class,
     
     // Usually not desirable:
-    Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector::class,
-    Rector\DeadCode\Rector\Assign\RemoveUnusedVariableAssignRector::class,
-    Rector\DeadCode\Rector\If_\RemoveAlwaysTrueIfConditionRector::class,
-    Rector\DeadCode\Rector\Switch_\RemoveDuplicatedCaseInSwitchRector::class,
-    Rector\CodeQuality\Rector\FunctionLike\SimplifyUselessVariableRector::class,
-    Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector::class,              // Harder to read code
-    Rector\TypeDeclaration\Rector\ClassMethod\ReturnNeverTypeRector::class,
-    Rector\Php54\Rector\Array_\LongArrayToShortArrayRector::class,                // Harder to read code
+    // ReturnNeverTypeRector::class,
     
-    // Sometimes not desirable:
-    Rector\Php80\Rector\Switch_\ChangeSwitchToMatchRector::class,
-    // Rector\Php71\Rector\FuncCall\RemoveExtraParametersRector::class,
+    /* SKIP AS NEEDED */
+    
+    // ShortenElseIfRector::class,
+    // AddLiteralSeparatorToNumberRector::class,
+    // RemoveUnusedVariableAssignRector::class,
+    // RemoveDuplicatedCaseInSwitchRector::class,
+    // SimplifyUselessVariableRector::class,
+    // SwitchNegatedTernaryRector::class,
+    // RemoveAlwaysTrueIfConditionRector::class,
+    // RemoveExtraParametersRector::class,
+    // LongArrayToShortArrayRector::class,
+    
+    /* ALWAYS SKIP */
+    
+    RemoveUnusedForeachKeyRector::class,                  // Nit-picking
+    SimplifyEmptyCheckOnEmptyArrayRector::class,          // Increases complexity
+    DisallowedEmptyRuleFixerRector::class,                // Nit-picking
+    VariableInStringInterpolationFixerRector::class,      // Breaks bash-code
+    ChangeSwitchToMatchRector::class,                     // Increases complexity
+    EncapsedStringsToSprintfRector::class,                // Why even
+    NewlineAfterStatementRector::class,                   // Get a life
+    SplitDoubleAssignRector::class,                       // Get a life
+    SymplifyQuoteEscapeRector::class,                     // Don't fix what ain't broken
+    WrapEncapsedVariableInCurlyBracesRector::class,       // Breaks bash-code
+    CompleteMissingIfElseBracketRector::class,            // Get a life
+    SingleInArrayToCompareRector::class,                  // Get a life
+    ChangeOrIfContinueToMultiContinueRector::class,       // Bullshit
+    DeclareStrictTypesRector::class,                      // Adds strict_types without checking?
   ]);
   
   // Register a single rule:
@@ -64,15 +121,15 @@ return static function (Rector\Config\RectorConfig $rectorConfig): void {
     // LevelSetList::UP_TO_PHP_81,
     // LevelSetList::UP_TO_PHP_82,
     LevelSetList::UP_TO_PHP_83,
-    SetList::DEAD_CODE,
-    SetList::CODE_QUALITY,
-    SetList::CODING_STYLE,
-    SetList::STRICT_BOOLEANS,
-    SetList::GMAGICK_TO_IMAGICK,
-    SetList::NAMING,
-    SetList::PRIVATIZATION,
-    SetList::EARLY_RETURN,
-    SetList::INSTANCEOF,
+    // SetList::DEAD_CODE,
+    // SetList::CODE_QUALITY,
+    // SetList::CODING_STYLE,
+    // SetList::STRICT_BOOLEANS,
+    // SetList::GMAGICK_TO_IMAGICK,
+    // SetList::NAMING,
+    // SetList::PRIVATIZATION,
+    // SetList::EARLY_RETURN,
+    // SetList::INSTANCEOF,
     
     // Broken:
     // SetList::TYPE_DECLARATION,
