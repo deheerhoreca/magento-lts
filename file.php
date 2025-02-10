f4fc8053f7bcf3cf27852a4864320fe8b60988b8
Loek <loek@deheerhoreca.nl>
2025-02-10 22:43:44 +0100

    update from production

--- file.php
+++ file.php
@@ -1,7 +1,7 @@
 <?php
 
 if(!isset($_GET["id"]) || empty((string) $_GET["id"])) {
-  header("Location: /", true, 301);
+  header("Location: /?csredir=omf", true, 301);
   exit;
 }
 
@@ -10,9 +10,9 @@ $iv             = str_repeat("a", openssl_cipher_iv_length($method));
 $url            = openssl_decrypt(base64_decode((string) $_GET["id"]), $method, "wijdelengeenurls", 0, $iv);
 
 if(empty($url)) {
-  header("Location: /", true, 301);
+  header("Location: /?csredir=omf", true, 301);
   exit;
 }
 
-header("Location: {$url}", true, 301);
+header("Location: {$url}", true, 302);
 exit;
