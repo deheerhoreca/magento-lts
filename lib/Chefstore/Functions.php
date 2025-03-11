73a2658f7c1f0cc12963135fc828778d04b34ac6
Loek <loek@deheerhoreca.nl>
2025-03-11 16:42:53 +0100

    update from production

--- lib/Chefstore/Functions.php
+++ lib/Chefstore/Functions.php
@@ -3,7 +3,7 @@
 // Product attributes only
 // Don't make $_product an (object) type hint
 if(!function_exists("om_attr_val")) {
-  function om_attr_val($_product, string $attribute_code, string $as = "", array $options = []) { // do not add type hint to $_product
+  function om_attr_val($_product, string $attribute_code, ?string $as = null, array $options = []) { // do not add type hint to $_product
     
     if(!is_object($_product)) {
       return null;
@@ -11,8 +11,7 @@ if(!function_exists("om_attr_val")) {
     
     if($_attribute = $_product->getResource()->getAttribute($attribute_code)) {
       $value = $_attribute->getFrontend()->getValue($_product);
-      
-      if($as) {
+      if(!blank($as)) {
         if($as === "string") return (string)  $value;
         if($as === "int")    return (int)     $value;
       }
@@ -21,7 +20,7 @@ if(!function_exists("om_attr_val")) {
     }
     
     $dhh_sku = $_product->getSku("dhh_sku") ?? "NO_DHH_SKU";  
-    logger("{$dhh_sku} Unknown attribute requested: {$attribute_code}", "WARNING");
+    Mage::log("{$dhh_sku} Unknown attribute requested: {$attribute_code}", Zend_Log::NOTICE);
     
     return false;
   }
