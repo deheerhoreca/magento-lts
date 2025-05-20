da28310fe0cb3e8dc85ef13f78d7e793dee0a6d8
Loek <loekvangool@gmail.com>
2025-05-20 09:47:11 +0000

    update from production

--- media/static/lease.php
+++ media/static/lease.php
@@ -9,7 +9,7 @@
         <div id="info-grenke" class="std mt-5">
           <img src="/skin/frontend/rwd/dhh/images/grenke_logo.png" alt="Grenke logo" title="Grenke" style="width:140px; height:auto; float:right; margin-top: .5em;">
           <h2>🤩Leasen met Grenke</h2>
-          <h5><i>Voor zakelijke klanten in Nederland en België</i></h5>
+          <h5><i>Voor zakelijke klanten in Nederland</i></h5>
           <h4>Maximaal €10.000 (zakelijk), incl. BTW</h4>
           <div style="max-width: 600px;">
             <strong>Met Grenke betaal je maandelijks een klein bedrag en ben je aan het einde van het contract de eigenaar van de apparatuur.</strong>
