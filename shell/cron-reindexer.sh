494f1f49a51bf6d0ed1362a688c188f83bf9f8b7
loek <loekvangool@gmail.com>
2025-06-05 23:58:59 +0000

    Update from MA

--- shell/cron-reindexer.sh
+++ shell/cron-reindexer.sh
@@ -2,17 +2,16 @@
 
 # ~/httpdocs/deheerhoreca-magento/shell/cron-reindexer.sh
 
+# Set User Environment
+. ${HOME}/.bash_profile
+
+cm && source ./shell/cron-bootstrap.sh
+
 # This runs on ma.deheerhoreca.nl only
 if [ "${HOSTNAME}" != "ma.deheerhoreca.nl" ] && [ "${HOSTNAME}" != "dev.deheerhoreca.nl" ]; then
   exit 0
 fi
 
-# Set User Environment
-. ${HOME}/.bash_profile
-
-set -e      # Exit immediately if a command exits with a non-zero status
-set -u      # Treat unset variables as an error when substituting
-
-cm
-
 mphp -c php.cmd.ini shell/indexer.php reindexallrequired
+
+cm && source ./shell/cron-wrapup.sh
