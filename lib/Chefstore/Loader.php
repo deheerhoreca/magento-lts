367e842c89dc818ffab9d4ab0260e4df3c0eff7d
Loek <loek@deheerhoreca.nl>
2026-02-04 23:59:56 +0000

    Update from MA

--- lib/Chefstore/Loader.php
+++ lib/Chefstore/Loader.php
@@ -8,7 +8,6 @@ use \Illuminate\Support\Arr;
 use \Illuminate\Support\Collection;
 use \Illuminate\Support\Number;
 use \Illuminate\Support\Str;
-use \Symfony\Contracts\Cache\ItemInterface;
 
 // Setup global aliases to prevent "use" statements all over -- Needs test because this file might get included multiple times by Composer
 // Cannot be executed multiple times between our apps
@@ -18,11 +17,10 @@ if (!defined("DHH_CLASS_ALIASES_APPLIED")) {
   if (!is_callable("Collection"))    class_alias(Collection::class, "Collection", true);
   if (!is_callable("Number"))        class_alias(Number::class, "Number", true);
   if (!is_callable("Str"))           class_alias(Str::class, "Str", true);
-  // if (!is_callable("ItemInterface")) class_alias(ItemInterface::class, "ItemInterface", true);
   define("DHH_CLASS_ALIASES_APPLIED", true);
 }
 
-require_once __DIR__."/functions.php"; // BEFORE config.php
+require_once __DIR__."/functions.php";          // BEFORE config.php
 require_once __DIR__."/config.php";
 require_once __DIR__."/CsCache.php";
 require_once __DIR__."/Helper.php";
