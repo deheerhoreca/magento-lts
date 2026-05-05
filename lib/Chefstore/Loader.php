c1f43624f3af5cf62287c553f3ff0693876ca8a3
Loek <loek@deheerhoreca.nl>
2026-05-05 21:23:48 +0000

    Update from MA

--- lib/Chefstore/Loader.php
+++ lib/Chefstore/Loader.php
@@ -8,18 +8,16 @@ declare(strict_types=1);
 // Cannot be executed multiple times between our apps
 // @todo DEPRECATED, move away from global aliases
 if (!defined("DHH_CLASS_ALIASES_APPLIED")) {
-  if(!class_exists("Arr"))                 class_alias(\Illuminate\Support\Arr::class, "Arr", true);
   if(!class_exists("Carbon"))              class_alias(\Carbon\Carbon::class, "Carbon", true);
+  if(!class_exists("Arr"))                 class_alias(\Illuminate\Support\Arr::class, "Arr", true);
   if(!class_exists("Collection"))          class_alias(\Illuminate\Support\Collection::class, "Collection", true);
   if(!class_exists("Number"))              class_alias(\Illuminate\Support\Number::class, "Number", true);
-  if(!class_exists("PrecisionStopwatch"))  class_alias(\Cspray\PrecisionStopwatch\Stopwatch::class, "PrecisionStopwatch", true);
   if(!class_exists("Str"))                 class_alias(\Illuminate\Support\Str::class, "Str", true);
   define("DHH_CLASS_ALIASES_APPLIED", true);
 }
 
 require_once __DIR__."/functions.php";          // BEFORE config.php
 require_once __DIR__."/config.php";
-
 require_once __DIR__."/CacheBuster.php";
 require_once __DIR__."/Catalog.php";
 require_once __DIR__."/CsCache.php";
